87500634e1365cdc3b595d3590e89e3bbae002e3
MrPowerUp82 <gustavohenrique8282@hotmail.com>
2024-07-10 15:30:43 -0300

    chore: Update console app with error handling for server status checking

--- console/apps.py
+++ console/apps.py
@@ -5,14 +5,17 @@ class ConsoleConfig(AppConfig):
     name = 'console'
 
     def ready(self) -> None:
-        print('Checking server status...')
-        from .views import is_server_running
-        from .models import Server
-        for server in Server.objects.all():
-            if is_server_running(server.id):
-                server.status = True
-                server.save()
-            else:
-                server.status = False
-                server.save()
-        print('Server status checked!')
+        try:
+            print('Checking server status...')
+            from .views import is_server_running
+            from .models import Server
+            for server in Server.objects.all():
+                if is_server_running(server.id):
+                    server.status = True
+                    server.save()
+                else:
+                    server.status = False
+                    server.save()
+            print('Server status checked!')
+        except Exception as e:
+            print(e)
