226a47b85b4c2cfac4c2ea578de4cc025163baa2
MrPowerUp82 <gustavohenrique8282@hotmail.com>
2024-07-13 13:32:15 -0300

    Update README.md and add installation instructions and example screenshots

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # mcmanager
+
+## Instalação
+
+  ```console
+sudo apt-get install openjdk-8-jre-headless
+  ```
+
+  ```console
+python3 -m pip install -r requirements.txt
+python3 manage.py generate_secret_key
+python3 manage.py migrate 
+python3 manage.py createsuperuser     
+  ```
+
+## Exemplo
+
+![alt text](screenshot.png)
+![alt text](screenshot2.png)
---
+++ console/management/commands/generate_secret_key.py
@@ -0,0 +1,30 @@
+import os
+import re
+from django.core.management.base import BaseCommand
+from django.core.management.utils import get_random_secret_key
+from django.conf import settings
+
+
+class Command(BaseCommand):
+    """
+    Gerar uma nova SECRET_KEY e atualizá-la no arquivo settings.py
+    """
+    help = 'Gerar uma nova SECRET_KEY e atualizá-la no arquivo settings.py'
+
+    def handle(self, *args, **kwargs):
+        secret_key = get_random_secret_key()
+        for root, dirs, files in os.walk(settings.BASE_DIR):
+            if 'settings.py' in files and 'asgi.py' in files and 'wsgi.py' in files:
+                settings_path = os.path.join(root, 'settings.py')
+                break
+        with open(settings_path, 'r') as file:
+            settings_content = file.read()
+        new_settings_content = re.sub(
+            r"^SECRET_KEY = ['\"].*['\"]", 
+            f"SECRET_KEY = '{secret_key}'", 
+            settings_content, 
+            flags=re.MULTILINE
+        )
+        with open(settings_path, 'w') as file:
+            file.write(new_settings_content)
+        self.stdout.write(self.style.SUCCESS(f'Nova SECRET_KEY gerada e atualizada no settings.py: {secret_key}'))
